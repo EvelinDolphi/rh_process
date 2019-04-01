c594fd8c6f00e3c2a38d62ad2d7aceb30eb22ee2
Evelin Castro <evelincastro021@gmail.com>
2019-04-01 00:49:31 -0600

    change the variables to null

--- database/migrations/2019_03_15_061438_create_clients_table.php
+++ database/migrations/2019_03_15_061438_create_clients_table.php
@@ -16,15 +16,15 @@ class CreateClientsTable extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 40);
-            $table->string('first_lastname', 20);
-            $table->string('second_lastname', 20);
-            $table->datetime('fecha_alta');
-            $table->datetime('antiguedad');
-            $table->string('sexo', 10);
-            $table->datetime('birthdate');
+            $table->string('first_lastname', 20)->nullable($value = true);
+            $table->string('second_lastname', 20)->nullable($value = true);
+            $table->datetime('fecha_alta')->nullable($value = true);
+            $table->datetime('antiguedad')->nullable($value = true);
+            $table->string('sexo', 10)->nullable($value = true);
+            $table->datetime('birthdate')->nullable($value = true);
             $table->string('numero_seguro', 15);
-            $table->string('puesto', 35);
-            $table->string('departamento', 35);
+            $table->string('puesto', 35)->nullable($value = true);
+            $table->string('departamento', 35)->nullable($value = true);
             $table->string('rfc', 35);
             $table->timestamps();
         });
